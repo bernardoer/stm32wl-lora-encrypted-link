8f42101a68e7ad6505f8cd46051cc406ddc3165f
Bernardoer <mr.bernardobvb@gmail.com>
2026-04-23 08:36:19 -0300

    experiment version with AES

--- TX/SubGHz_Phy_PingPong/SubGHz_Phy/App/subghz_phy_app.c
+++ TX/SubGHz_Phy_PingPong/SubGHz_Phy/App/subghz_phy_app.c
@@ -25,11 +25,7 @@
 #include "radio.h"
 
 /* USER CODE BEGIN Includes */
-#include "stm32_timer.h"
 #include "stm32_seq.h"
-#include "utilities_def.h"
-#include "app_version.h"
-#include "subghz_phy_version.h"
 #include "aes.h"
 #include <string.h>
 /* USER CODE END Includes */
@@ -45,18 +41,12 @@ extern char tx_message[64];
 /* USER CODE BEGIN PTD */
 typedef enum
 {
-  RX,
-  RX_TIMEOUT,
-  RX_ERROR,
-  TX,
-  TX_TIMEOUT,
-} States_t;
+	APP_SEND_FIRST,
+	APP_SEND_NEXT,
+	APP_RETRY
+} AppState_t;
 
-typedef struct
-{
-  uint32_t counter;
-  uint8_t ciphertext[16];
-} TxPacket_t;
+static AppState_t AppState = APP_SEND_FIRST;
 
 /* USER CODE END PTD */
 
@@ -64,20 +54,14 @@ typedef struct
 /* USER CODE BEGIN PD */
 /* Configurations */
 /*Timeout*/
-#define RX_TIMEOUT_VALUE              3000
 #define TX_TIMEOUT_VALUE              3000
-/*Size of the payload to be sent*/
 /* Size must be greater of equal the PING and PONG*/
 #define MAX_APP_BUFFER_SIZE          255
-#if (PAYLOAD_LEN > MAX_APP_BUFFER_SIZE)
-#error PAYLOAD_LEN must be less or equal than MAX_APP_BUFFER_SIZE
-#endif /* (PAYLOAD_LEN > MAX_APP_BUFFER_SIZE) */
-/* wait for remote to be in Rx, before sending a Tx frame*/
-#define RX_TIME_MARGIN                200
-/* Afc bandwidth in Hz */
-#define FSK_AFC_BANDWIDTH             83333
-/* LED blink Period*/
-#define LED_PERIOD_MS                 200
+/* Period between each message*/
+#define SEND_PERIOD_MS 3000
+
+#define TX_COUNTER_SIZE 4U
+#define TX_MAX_MESSAGE_SIZE 64U
 
 /* USER CODE END PD */
 
@@ -91,29 +75,10 @@ typedef struct
 static RadioEvents_t RadioEvents;
 
 /* USER CODE BEGIN PV */
-/*Ping Pong FSM states */
-static States_t State = RX;
-/* App Rx Buffer*/
-static uint8_t BufferRx[MAX_APP_BUFFER_SIZE];
 /* App Tx Buffer*/
 static uint8_t BufferTx[MAX_APP_BUFFER_SIZE];
 /* Last  Received Buffer Size*/
-uint16_t RxBufferSize = 0;
-/* Last  Received packer Rssi*/
-int8_t RssiValue = 0;
-/* Last  Received packer SNR (in Lora modulation)*/
-int8_t SnrValue = 0;
-/* Led Timers objects*/
-static UTIL_TIMER_Object_t timerLed;
-/* device state. Master: true, Slave: false*/
-bool isMaster = true;
-/* random delay to make sure 2 devices will sync*/
-/* the closest the random delays are, the longer it will
-   take for the devices to sync when started simultaneously*/
 static uint32_t txCounter = 0;
-
-static int32_t random_delay;
-
 
 /* USER CODE END PV */
 
@@ -122,52 +87,40 @@ static int32_t random_delay;
  * @brief Function to be executed on Radio Tx Done event
  */
 static void OnTxDone(void);
-
-/**
-  * @brief Function to be executed on Radio Rx Done event
-  * @param  payload ptr of buffer received
-  * @param  size buffer size
-  * @param  rssi
-  * @param  LoraSnr_FskCfo
-  */
-static void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t LoraSnr_FskCfo);
-
 /**
   * @brief Function executed on Radio Tx Timeout event
   */
 static void OnTxTimeout(void);
 
-/**
-  * @brief Function executed on Radio Rx Timeout event
-  */
-static void OnRxTimeout(void);
-
-/**
-  * @brief Function executed on Radio Rx Error event
-  */
-static void OnRxError(void);
 
 /* USER CODE BEGIN PFP */
 /**
-  * @brief  Function executed on when led timer elapses
-  * @param  context ptr of LED context
+  * @brief  AES set counter
   */
-static void OnledEvent(void *context);
+static void AES_LoadCounter(uint32_t counter);
 
 /**
-  * @brief PingPong state machine implementation
+  * @brief Tx prepare payload
   */
-static void PingPong_Process(void);
+static uint8_t PrepareTxPayload(void);
 
 /**
-  * @brief PingPong TX configure and process
+  * @brief  TX configure and process send
   */
 static void RadioSend(uint8_t size);
 
 /**
-  * @brief PingPong RX configure and process
+  * @brief TX configure and process
   */
-static void RadioRx(void);
+static void AppProcess(void);
+
+/**
+  * @brief Call the send of the packet
+  */
+static void SendNow(void);
+
+static void DWT_Init(void);
+
 
 /* USER CODE END PFP */
 
@@ -175,68 +128,29 @@ static void RadioRx(void);
 void SubghzApp_Init(void)
 {
   /* USER CODE BEGIN SubghzApp_Init_1 */
-
-  APP_LOG(TS_OFF, VLEVEL_M, "\n\rPING PONG\n\r");
-  /* Get SubGHY_Phy APP version*/
-  APP_LOG(TS_OFF, VLEVEL_M, "APPLICATION_VERSION: V%X.%X.%X\r\n",
-          (uint8_t)(APP_VERSION_MAIN),
-          (uint8_t)(APP_VERSION_SUB1),
-          (uint8_t)(APP_VERSION_SUB2));
-
-  /* Get MW SubGhz_Phy info */
-  APP_LOG(TS_OFF, VLEVEL_M, "MW_RADIO_VERSION:    V%X.%X.%X\r\n",
-          (uint8_t)(SUBGHZ_PHY_VERSION_MAIN),
-          (uint8_t)(SUBGHZ_PHY_VERSION_SUB1),
-          (uint8_t)(SUBGHZ_PHY_VERSION_SUB2));
-
-  /* Led Timers*/
-  UTIL_TIMER_Create(&timerLed, LED_PERIOD_MS, UTIL_TIMER_ONESHOT, OnledEvent, NULL);
-  UTIL_TIMER_Start(&timerLed);
-  /* USER CODE END SubghzApp_Init_1 */
+  APP_LOG(TS_OFF, VLEVEL_M, "\n\rTX APP\n\r");
 
   /* Radio initialization */
   RadioEvents.TxDone = OnTxDone;
-  RadioEvents.RxDone = OnRxDone;
   RadioEvents.TxTimeout = OnTxTimeout;
-  RadioEvents.RxTimeout = OnRxTimeout;
-  RadioEvents.RxError = OnRxError;
+  RadioEvents.RxDone = NULL;
+  RadioEvents.RxTimeout = NULL;
+  RadioEvents.RxError = NULL;
 
   Radio.Init(&RadioEvents);
   MX_AES_Init();
+  DWT_Init();
 
   /* USER CODE BEGIN SubghzApp_Init_2 */
-  /*calculate random delay for synchronization*/
-  random_delay = (Radio.Random()) >> 22; /*10bits random e.g. from 0 to 1023 ms*/
 
   /* Radio Set frequency */
   Radio.SetChannel(RF_FREQUENCY);
 
-  /* Radio configuration */
-#if ((USE_MODEM_LORA == 1) && (USE_MODEM_FSK == 0))
-  APP_LOG(TS_OFF, VLEVEL_M, "---------------\n\r");
-  APP_LOG(TS_OFF, VLEVEL_M, "LORA_MODULATION\n\r");
-  APP_LOG(TS_OFF, VLEVEL_M, "LORA_BW=%d kHz\n\r", (1 << LORA_BANDWIDTH) * 125);
-  APP_LOG(TS_OFF, VLEVEL_M, "LORA_SF=%d\n\r", LORA_SPREADING_FACTOR);
-#elif ((USE_MODEM_LORA == 0) && (USE_MODEM_FSK == 1))
-  APP_LOG(TS_OFF, VLEVEL_M, "---------------\n\r");
-  APP_LOG(TS_OFF, VLEVEL_M, "FSK_MODULATION\n\r");
-  APP_LOG(TS_OFF, VLEVEL_M, "FSK_BW=%d Hz\n\r", FSK_BANDWIDTH);
-  APP_LOG(TS_OFF, VLEVEL_M, "FSK_DR=%d bits/s\n\r", FSK_DATARATE);
-#else
-#error "Please define a modulation in the subghz_phy_app.h file."
-#endif /* USE_MODEM_LORA | USE_MODEM_FSK */
-
-  /*fills tx buffer*/
-  memset(BufferTx, 0x0, MAX_APP_BUFFER_SIZE);
-
-  APP_LOG(TS_ON, VLEVEL_L, "rand=%d\n\r", random_delay);
-
   /*register task to to be run in while(1) after Radio IT*/
-  UTIL_SEQ_RegTask((1 << CFG_SEQ_Task_SubGHz_Phy_App_Process), UTIL_SEQ_RFU, PingPong_Process);
+  UTIL_SEQ_RegTask((1 << CFG_SEQ_Task_SubGHz_Phy_App_Process),
+                     UTIL_SEQ_RFU,
+                     AppProcess);
 
-  /*starts first process */
-  HAL_Delay(1000);
-  State = RX_TIMEOUT;
   UTIL_SEQ_SetTask((1 << CFG_SEQ_Task_SubGHz_Phy_App_Process), CFG_SEQ_Prio_0);
   /* USER CODE END SubghzApp_Init_2 */
 }
@@ -249,89 +163,29 @@ void SubghzApp_Init(void)
 static void OnTxDone(void)
 {
   /* USER CODE BEGIN OnTxDone */
-  APP_LOG(TS_ON, VLEVEL_L, "OnTxDone\n\r");
+  APP_LOG(TS_ON, VLEVEL_L, "Tx Done\n\r");
   /* Update the State of the FSM*/
-  State = TX;
+  AppState = APP_SEND_NEXT;
   /* Run PingPong process in background*/
   UTIL_SEQ_SetTask((1 << CFG_SEQ_Task_SubGHz_Phy_App_Process), CFG_SEQ_Prio_0);
   /* USER CODE END OnTxDone */
 }
 
-static void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t LoraSnr_FskCfo)
-{
-  /* USER CODE BEGIN OnRxDone */
-  APP_LOG(TS_ON, VLEVEL_L, "OnRxDone\n\r");
-#if ((USE_MODEM_LORA == 1) && (USE_MODEM_FSK == 0))
-  APP_LOG(TS_ON, VLEVEL_L, "RssiValue=%d dBm, SnrValue=%ddB\n\r", rssi, LoraSnr_FskCfo);
-  /* Record payload Signal to noise ratio in Lora*/
-  SnrValue = LoraSnr_FskCfo;
-#endif /* USE_MODEM_LORA | USE_MODEM_FSK */
-#if ((USE_MODEM_LORA == 0) && (USE_MODEM_FSK == 1))
-  APP_LOG(TS_ON, VLEVEL_L, "RssiValue=%d dBm, Cfo=%dkHz\n\r", rssi, LoraSnr_FskCfo);
-  SnrValue = 0; /*not applicable in GFSK*/
-#endif /* USE_MODEM_LORA | USE_MODEM_FSK */
-  /* Update the State of the FSM*/
-  State = RX;
-  /* Clear BufferRx*/
-  memset(BufferRx, 0, MAX_APP_BUFFER_SIZE);
-  /* Record payload size*/
-  RxBufferSize = size;
-  if (RxBufferSize <= MAX_APP_BUFFER_SIZE)
-  {
-    memcpy(BufferRx, payload, RxBufferSize);
-  }
-  /* Record Received Signal Strength*/
-  RssiValue = rssi;
-  /* Record payload content*/
-  APP_LOG(TS_ON, VLEVEL_H, "payload. size=%d \n\r", size);
-  for (int32_t i = 0; i < PAYLOAD_LEN; i++)
-  {
-    APP_LOG(TS_OFF, VLEVEL_H, "%02X", BufferRx[i]);
-    if (i % 16 == 15)
-    {
-      APP_LOG(TS_OFF, VLEVEL_H, "\n\r");
-    }
-  }
-  APP_LOG(TS_OFF, VLEVEL_H, "\n\r");
-  /* Run PingPong process in background*/
-  UTIL_SEQ_SetTask((1 << CFG_SEQ_Task_SubGHz_Phy_App_Process), CFG_SEQ_Prio_0);
-  /* USER CODE END OnRxDone */
-}
-
 static void OnTxTimeout(void)
 {
   /* USER CODE BEGIN OnTxTimeout */
-  APP_LOG(TS_ON, VLEVEL_L, "OnTxTimeout\n\r");
+  APP_LOG(TS_ON, VLEVEL_L, "Tx Timeout\n\r");
   /* Update the State of the FSM*/
-  State = TX_TIMEOUT;
+  AppState = APP_RETRY;
   /* Run PingPong process in background*/
   UTIL_SEQ_SetTask((1 << CFG_SEQ_Task_SubGHz_Phy_App_Process), CFG_SEQ_Prio_0);
   /* USER CODE END OnTxTimeout */
 }
 
-static void OnRxTimeout(void)
-{
-  /* USER CODE BEGIN OnRxTimeout */
-  APP_LOG(TS_ON, VLEVEL_L, "OnRxTimeout\n\r");
-  /* Update the State of the FSM*/
-  State = RX_TIMEOUT;
-  /* Run PingPong process in background*/
-  UTIL_SEQ_SetTask((1 << CFG_SEQ_Task_SubGHz_Phy_App_Process), CFG_SEQ_Prio_0);
-  /* USER CODE END OnRxTimeout */
-}
 
-static void OnRxError(void)
-{
-  /* USER CODE BEGIN OnRxError */
-  APP_LOG(TS_ON, VLEVEL_L, "OnRxError\n\r");
-  /* Update the State of the FSM*/
-  State = RX_ERROR;
-  /* Run PingPong process in background*/
-  UTIL_SEQ_SetTask((1 << CFG_SEQ_Task_SubGHz_Phy_App_Process), CFG_SEQ_Prio_0);
-  /* USER CODE END OnRxError */
-}
+/* USER CODE BEGIN PrFD */
 
-static void AES_SetCounter(uint32_t counter)
+static void AES_LoadCounter(uint32_t counter)
 {
   static uint32_t iv[4];
 
@@ -349,7 +203,81 @@ static void AES_SetCounter(uint32_t counter)
   }
 }
 
-/* USER CODE BEGIN PrFD */
+static uint8_t PrepareTxPayload(void)
+{
+  /* time measure */
+
+  uint32_t prepare_start, prepare_end, prepare_cycles;
+  uint32_t enc_start, enc_end, enc_cycles;
+
+  APP_LOG(TS_ON, VLEVEL_M, "message: %s\r\n", tx_message);
+  uint32_t counter = txCounter++;
+  APP_LOG(TS_ON, VLEVEL_M, "counter: %u\r\n", (unsigned int)counter);
+
+  prepare_start = DWT->CYCCNT;
+
+  uint8_t msg_len = (uint8_t)strnlen(tx_message, TX_MAX_MESSAGE_SIZE);
+  uint8_t enc_len = (uint8_t)(((msg_len + 15U) / 16U) * 16U);
+
+  uint8_t plain[TX_MAX_MESSAGE_SIZE] = {0};
+  uint8_t cipher[TX_MAX_MESSAGE_SIZE] = {0};
+
+  if (msg_len == 0U)
+  {
+    APP_LOG(TS_ON, VLEVEL_M, "Empty message\r\n");
+    return 0;
+  }
+
+  memcpy(plain, tx_message, msg_len);
+
+  AES_LoadCounter(counter);
+
+  enc_start = DWT->CYCCNT;
+
+  if (HAL_CRYP_Encrypt(&hcryp,
+                       (uint32_t *)plain,
+                       enc_len / 4U,
+                       (uint32_t *)cipher,
+                       HAL_MAX_DELAY) != HAL_OK)
+  {
+    APP_LOG(TS_ON, VLEVEL_M, "AES encrypt error\r\n");
+    return 0;
+  }
+
+  enc_end = DWT->CYCCNT;
+  enc_cycles = enc_end - enc_start;
+
+  memcpy(BufferTx, &counter, TX_COUNTER_SIZE);
+  memcpy(BufferTx + TX_COUNTER_SIZE, cipher, enc_len);
+
+  prepare_end = DWT->CYCCNT;
+
+  prepare_cycles = prepare_end - prepare_start;
+
+  APP_LOG(TS_ON, VLEVEL_M, "enc_cycles: %u\r\n", (unsigned int)enc_cycles);
+  APP_LOG(TS_ON, VLEVEL_M, "prepare_cycles: %u\r\n", (unsigned int)prepare_cycles);
+
+  uint64_t enc_01us = ((uint64_t)enc_cycles * 100000000ULL) / SystemCoreClock;
+  uint64_t prepare_01us = ((uint64_t)prepare_cycles * 100000000ULL) / SystemCoreClock;
+
+  APP_LOG(TS_ON, VLEVEL_M, "enc_time_us: %u.%02u\r\n",
+          (unsigned int)(enc_01us / 100ULL),
+          (unsigned int)(enc_01us % 100ULL));
+
+  APP_LOG(TS_ON, VLEVEL_M, "prepare_time_us: %u.%02u\r\n",
+          (unsigned int)(prepare_01us / 100ULL),
+          (unsigned int)(prepare_01us % 100ULL));
+
+  APP_LOG(TS_ON, VLEVEL_M, "encrypted message: ");
+  for (uint8_t i = 0; i < enc_len; i++)
+  {
+    APP_LOG(TS_OFF, VLEVEL_M, "%02X", BufferTx[TX_COUNTER_SIZE + i]);
+  }
+  APP_LOG(TS_OFF, VLEVEL_M, "\r\n");
+
+  return (uint8_t)(TX_COUNTER_SIZE + enc_len);
+}
+
 static void RadioSend(uint8_t size)
 {
 #if ((USE_MODEM_LORA == 1) && (USE_MODEM_FSK == 0))
@@ -360,162 +288,48 @@ static void RadioSend(uint8_t size)
                     LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
                     true, 0, 0, LORA_IQ_INVERSION_ON, TX_TIMEOUT_VALUE);
   Radio.SetMaxPayloadLength(MODEM_LORA, MAX_APP_BUFFER_SIZE);
-#elif ((USE_MODEM_LORA == 0) && (USE_MODEM_FSK == 1))
-  Radio.SetTxConfig(MODEM_FSK, TX_OUTPUT_POWER, FSK_FDEV, 0,
-                    FSK_DATARATE, 0,
-                    FSK_PREAMBLE_LENGTH, FSK_FIX_LENGTH_PAYLOAD_ON,
-                    true, 0, 0, 0, TX_TIMEOUT_VALUE);
-  Radio.SetMaxPayloadLength(MODEM_FSK, MAX_APP_BUFFER_SIZE);
 #else
-#error "Please define a modulation in the subghz_phy_app.h file."
+#error "Pnly LoRa mode handled in this simplified version"
 #endif /* USE_MODEM_LORA | USE_MODEM_FSK */
-
-  APP_LOG(TS_ON, VLEVEL_M, "encrypted message: ");
-    for (uint8_t i = 0; i < size; i++)
-    {
-      APP_LOG(TS_OFF, VLEVEL_M, "%02X", BufferTx[i]);
-    }
-  APP_LOG(TS_OFF, VLEVEL_M, "\r\n");
 
   Radio.Send(BufferTx, size);
 }
 
-static void RadioRx(void)
+static void SendNow(void)
 {
-#if ((USE_MODEM_LORA == 1) && (USE_MODEM_FSK == 0))
-  Radio.Sleep();
-  Radio.SetChannel(RF_FREQUENCY);
-  Radio.SetRxConfig(MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
-                    LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
-                    LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
-                    0, true, 0, 0, LORA_IQ_INVERSION_ON, true);
-  if (LORA_FIX_LENGTH_PAYLOAD_ON == true)
-  {
-    Radio.SetMaxPayloadLength(MODEM_LORA, PAYLOAD_LEN);
-  }
-  else
-  {
-    Radio.SetMaxPayloadLength(MODEM_LORA, MAX_APP_BUFFER_SIZE);
-  }
-#elif ((USE_MODEM_LORA == 0) && (USE_MODEM_FSK == 1))
-  Radio.SetRxConfig(MODEM_FSK, FSK_BANDWIDTH, FSK_DATARATE,
-                    0, FSK_AFC_BANDWIDTH, FSK_PREAMBLE_LENGTH,
-                    0, FSK_FIX_LENGTH_PAYLOAD_ON, 0, true,
-                    0, 0, false, true);
-  if (LORA_FIX_LENGTH_PAYLOAD_ON == true)
-  {
-    Radio.SetMaxPayloadLength(MODEM_FSK, PAYLOAD_LEN);
-  }
-  else
-  {
-    Radio.SetMaxPayloadLength(MODEM_FSK, MAX_APP_BUFFER_SIZE);
-  }
-#else
-#error "Please define a modulation in the subghz_phy_app.h file."
-#endif /* USE_MODEM_LORA | USE_MODEM_FSK */
-
-  Radio.Rx(RX_TIMEOUT_VALUE);
+    uint8_t size = PrepareTxPayload();
+    if(size > 0)
+    {
+        RadioSend(size);
+    }
 }
 
-static uint8_t PrepareTxPayload(void)
+static void AppProcess(void)
 {
-  uint32_t counter = txCounter++;
-  uint8_t msg_len = strlen(tx_message);
-  uint8_t padded_len = ((msg_len + 15) / 16) * 16;
-
-  uint8_t plain[64] = {0};
-  uint8_t cipher[64] = {0};
-
-  APP_LOG(TS_ON, VLEVEL_M, "message: %s\r\n", tx_message);
-  APP_LOG(TS_ON, VLEVEL_M, "counter: %u\r\n", (unsigned int)counter);
-
-  memset(BufferTx, 0, MAX_APP_BUFFER_SIZE);
-  memcpy(plain, tx_message, msg_len);
-
-  /* reset AES with IV/counter for this packet */
-  MX_AES_Init();
-  AES_SetCounter(counter);
-
-  /* encrypt one AES block */
-  if (HAL_CRYP_Encrypt(&hcryp,
-		  	  	  	    (uint32_t *)plain,
-						padded_len / 4,
-						(uint32_t *) cipher,
-						HAL_MAX_DELAY) != HAL_OK)
+  switch (AppState)
   {
-    APP_LOG(TS_ON, VLEVEL_M, "AES encrypt error\r\n");
-    return 0;
-  }
-
-  memcpy(&BufferTx[0], &counter, 4);
-  memcpy(&BufferTx[4], cipher, padded_len);
-
-  APP_LOG(TS_ON, VLEVEL_M, "encrypted message: ");
-  for (uint8_t i = 0; i < padded_len; i++)
-  {
-    APP_LOG(TS_OFF, VLEVEL_M, "%02X", BufferTx[4 + i]);
-  }
-  APP_LOG(TS_OFF, VLEVEL_M, "\r\n");
-
-  return 4 + padded_len;
-}
-
-static void PingPong_Process(void)
-{
-  uint8_t txsize;
-
-  Radio.Sleep();
-
-  switch (State)
-  {
-    case RX:
-      APP_LOG(TS_ON, VLEVEL_M, "TX node unexpectedly received something\r\n");
-      HAL_Delay(500);
-      RadioRx();
+    case APP_SEND_FIRST:
+      APP_LOG(TS_ON, VLEVEL_M, "First send\r\n");
+      SendNow();
       break;
 
-    case TX:
-      APP_LOG(TS_ON, VLEVEL_M, "TX done, waiting before next send\r\n");
-      HAL_Delay(3000);
-      txsize = PrepareTxPayload();
-      RadioSend(txsize);
+    case APP_SEND_NEXT:
+      HAL_Delay(SEND_PERIOD_MS);
+      SendNow();
       break;
 
-    case RX_TIMEOUT:
-      APP_LOG(TS_ON, VLEVEL_M, "Initial TX start\r\n");
-      HAL_Delay(500);
-      txsize = PrepareTxPayload();
-      RadioSend(txsize);
-      break;
-
-    case RX_ERROR:
-      APP_LOG(TS_ON, VLEVEL_M, "RX error on TX node, sending again\r\n");
-      HAL_Delay(500);
-      txsize = PrepareTxPayload();
-      RadioSend(txsize);
-      break;
-
-    case TX_TIMEOUT:
-      APP_LOG(TS_ON, VLEVEL_M, "TX timeout, retrying\r\n");
+    case APP_RETRY:
       HAL_Delay(1000);
-      txsize = PrepareTxPayload();
-      RadioSend(txsize);
-      break;
-
-    default:
-      APP_LOG(TS_ON, VLEVEL_M, "Unknown state, sending again\r\n");
-      HAL_Delay(500);
-      txsize = PrepareTxPayload();
-      RadioSend(txsize);
+      SendNow();
       break;
   }
 }
 
-static void OnledEvent(void *context)
+static void DWT_Init(void)
 {
-  HAL_GPIO_TogglePin(LED2_GPIO_Port, LED2_Pin); /* LED_GREEN */
-  HAL_GPIO_TogglePin(LED3_GPIO_Port, LED3_Pin); /* LED_RED */
-  UTIL_TIMER_Start(&timerLed);
+    CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+    DWT->CYCCNT = 0;
+    DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
 }
 
 /* USER CODE END PrFD */
